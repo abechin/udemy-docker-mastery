b776d88ba640762eb4fdfe56ad618ab201ab07f0
abe.chin <abe.chin@servicenow.com>
2018-04-27 09:56:52 -0700

    MAINT: Add name volume assignment and answer.

---
+++ assignment/section_4/assignment_1/README.md
@@ -0,0 +1,17 @@
+# Assignment: Named Volumes #
+
+* Database upgrade with containers
+* Create a postgres container with named volume psql-data using version 9.6.1
+* Use Docker Hub to learn VOLUME path and versions needed to run it
+* Check logs, **stop container**
+* Create a new postgres container with same named volume using 9.6.2
+* Check logs to validate
+
+## Note ##
+
+* <span style="color:blue">This only works with patch versions, most SQL DB's require manual commands to upgrade DB's to major/minor versions, i.e. it's a DB limitation not a container one</span>
+
+## Reference ##
+
+* [postgres - dockerhub official repository](https://hub.docker.com/_/postgres/)
+* [postgress 9.6 Dockerfile](https://github.com/docker-library/postgres/blob/ef4545c07bd97e5585bb9e7f2680a4859b9ccf3c/9.6/Dockerfile)
---
+++ assignment/section_4/assignment_1/create_named_volume_container.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+create_volume() {
+  name=$1
+  docker volume create $name
+}
+
+create_postgres() {
+  version=$1
+  volume_name=$2
+  echo "##########"
+  echo "docker container run -it --name postgres_${version} -v ${volume_name}:/var/lib/postgresql/data postgres:${version}"
+  echo "##########"
+  docker container run -it --name postgres_${version} -v ${volume_name}:/var/lib/postgresql/data postgres:${version}
+}
+
+log_postgres() {
+  img_name=$1
+  echo "##########"
+  echo "docker logs ${img_name}"
+  echo "##########"
+  docker logs ${img_name}
+}
+
+list_volume() {
+  name=$1
+  echo "##########"
+  echo "docker volume ls | grep ${name}"
+  echo "##########"
+  docker volume ls | grep ${name}
+}
+
+stop_container() {
+  img_name=$1
+  echo "##########"
+  echo "docker container stop ${img_name}"
+  echo "##########"
+  docker container stop ${img_name}
+}
+
+delete_container() {
+  img_name=$1
+  echo "##########"
+  echo "docker container rm ${img_name}"
+  echo "##########"
+  docker container rm ${img_name}
+}
+
+delete_volume() {
+  volume_name=$1
+  echo "##########"
+  echo "docker volume rm ${volume_name}"
+  echo "##########"
+  docker volume rm ${volume_name}
+}
+
+main() {
+  local volume_name version
+
+  volume_name='psql-data'
+
+  # Create volume with name psql-data
+  create_volume ${volume_name}
+
+  # Create postgres 9.6.1 container
+  version='9.6.1'
+  create_postgres ${version} ${volume_name}
+
+  # Display postgres log
+  log_postgres postgres_${version}
+
+  # List volume name
+  list_volume ${volume_name}
+
+  # Stop postgres container
+  stop_container postgres_${version}
+
+  # Delete postgres container
+  delete_container postgres_${version}
+
+  # Create postgres 9.6.2 container
+  version='9.6.2'
+  create_postgres ${version} ${volume_name}
+
+  # Display postgres log
+  log_postgres postgres_${version}
+
+  # List volume name
+  list_volume ${volume_name}
+
+  # Stop postgres container
+  stop_container postgres_${version}
+
+  # Delete postgres container
+  delete_container postgres_${version}
+
+  # Delete volume
+  delete_volume ${volume_name}
+}
+
+main "$@"
